9a01fc3e5f72a1b68e27116a7b1f3dffbaf8c30c
Angel David Parra <angdavdeveloper@gmail.com>
2025-05-09 19:13:32 +0200

    new project

---
+++ src/main.js
@@ -0,0 +1,2 @@
+import './sass/estilos.scss'
+
---
+++ vite.config.js
@@ -0,0 +1,5 @@
+import { defineConfig } from "vite";
+
+export default defineConfig( {
+    base: "new_project", // Usa rutas relativas en los enlaces generados en el html
+})
